806e3d5802ac42d97016edbda0bcfcbbdde58bc5
Rusheeta <31394120+Rusheeta@users.noreply.github.com>
2017-08-27 23:37:20 -0500

    Add files via upload

---
+++ 4.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int N,i;
+float Num,ADD=0,AVG;
+clrscr();
+printf("Enter how many numbers you need:");
+scanf("%d",&N);
+printf("Enter N numbers:\n");
+for(i=1;i<=N;i++)
+{
+scanf("%f",&Num);
+ADD=Num+ADD;
+}
+AVG=ADD/N;
+printf("Average of N numbers is %f",AVG);
+getch();
+}
