c7dee8fc34fb6ba3cb92f92d29bfe4472edabb40
Rusheeta <31394120+Rusheeta@users.noreply.github.com>
2017-10-25 01:50:07 -0500

    Version 1

---
+++ 22-Sep-2017/singleLinkedList.c
@@ -0,0 +1,117 @@
+#include<stdio.h>
+#include<malloc.h>
+
+typedef struct node 
+    {
+       int data ;
+       struct node *next ;
+    } NODE ;
+    
+    /* Declare pointers of the list as a Global Variable */
+    
+    NODE  *head= NULL ;   
+    NODE  *scanPtr= NULL ; 
+    
+    
+	int DisplayMenu()
+    {
+    	int option;
+    	
+    	printf("\nMenu:\n");
+    	printf("1.Create List\n");
+    	printf("2.Insert Node\n");
+    	printf("3.Delete Node\n");
+    	printf("4.Display List\n");
+    	printf("5.Delete List\n");
+    	printf("6.Search List\n");
+    	printf("7.Exit\n");
+    	
+    	printf("Choose an option to perform the action:\n");
+    	scanf("%d",&option);
+    	
+		return option;    	
+	}
+	
+    void CreateList()
+    {
+    				    	
+	}
+	
+	void InsertNode()
+	{
+	
+	}
+	
+	void DeleteNode()
+	{		
+		
+	}
+	
+	void DisplayList()
+	{
+		printf("List is Empty\n");
+	}
+	
+	void DeleteList()
+	{
+	
+	}
+	
+	void SearchList()
+	{
+				
+	}	
+	
+    
+    int main()
+    {
+        int flag ;
+        int choice ;
+        flag = 1 ;
+        
+        while (flag ==1)
+        {
+             choice = DisplayMenu() ;
+             switch(choice)
+             {  case 1 :
+                { 
+                     CreateList() ;
+                     break ;
+                }
+                case 2:
+                {
+                     InsertNode();
+                     break ;
+                }
+                case 3:
+                {
+                    DeleteNode();
+                    break ;
+                }
+                case 4:
+                {
+                    DisplayList() ;
+                    break ;
+                }
+                case 5:
+                {
+                    DeleteList() ; /* traverse through the list and remove each element by element */
+                    break ;
+                }
+                case 6:
+                {
+                    SearchList();
+                    break ;
+                }
+                case 7:
+                {
+                    flag = 0 ;
+                    break ;
+                }
+                default:
+                {
+                   break ;
+                }
+             }
+        }
+    }
